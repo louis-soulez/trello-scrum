5e12a5ff85f7a0034f5765ce5bd43ee52a817dbe
Louis Soulez <louis.soulez@navya.tech>
2017-06-16 14:48:20 +0200

    Make use of external jq script

--- extract.sh
+++ extract.sh
@@ -78,7 +78,7 @@ function parse {
   fi
 
   cat $INPUT \
-  | jq "{} as \$idx | .lists as \$l | .cards as \$c | [ \$c[] | (.name | split(\" | \")) as \$n | { name: \$n[0], cost: (if \$n[1] == null then \"\" else \$n[1] end), list: (.idList as \$i | \$l[] | select ( .id == \$i )) | {name: .name, closed: .closed}, labels: [ .labels[] | { name: .name, color: (if .color == \"sky\" then \"cyan\" else .color end)} ], desc: .desc, closed: .closed} | select(.closed == false $filter) ]" \
+  | "$ROOT/extract.card.sh" "$filter" \
   | jq -r '@json "\(.[])"' \
   | export
 
